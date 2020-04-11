b89940ce1a78a48eda26665f59ac29d15d73586b
solidaridad\ra.garcia <rgarcia@evopointsolution.com>
2020-04-11 13:49:04 -0400

    Unit Test for ChatController

--- SimpleChat/SimpleChat.Test/Controller/ChatControllerTest.cs
+++ SimpleChat/SimpleChat.Test/Controller/ChatControllerTest.cs
@@ -1,11 +1,44 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace SimpleChat.Test.Controller
 {
     public class ChatControllerTest
     {
+        public readonly Mock<Fakes.FakeSimpletChatDbContext> _context;
+        public readonly Mock<Fakes.FakeUserManager> _userManager;
 
+        public ChatControllerTest()
+        {
+            _context = new Mock<Fakes.FakeSimpletChatDbContext>();
+            _userManager = new Mock<Fakes.FakeUserManager>();
+        }
+
+        [Fact]
+        public void Index_Returns_View()
+        {
+            //Arrange
+            var controller = new SimpleChat.Web.Controllers.ChatController(_context.Object, _userManager.Object);
+            //Act
+            var result = controller.Index();
+            //Assert
+            Assert.IsAssignableFrom<Task<Microsoft.AspNetCore.Mvc.IActionResult>>(result);
+            
+        }
+
+        [Fact]
+        public void Create_Returns_JsonResult()
+        {
+            //Arrange
+            var controller = new SimpleChat.Web.Controllers.ChatController(_context.Object, _userManager.Object);
+            //Act
+            var result = controller.Create("TEST");
+            //Assert
+            Assert.IsAssignableFrom<Task<Microsoft.AspNetCore.Mvc.JsonResult>>(result);
+        }
     }
 }
---
+++ SimpleChat/SimpleChat.Test/Fakes/FakeSimpletChatDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SimpleChat.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChat.Test.Fakes
+{
+    public class FakeSimpletChatDbContext : SimpleChat.Web.Data.SimpleChatDBContext
+    {
+        public FakeSimpletChatDbContext() : base(new DbContextOptions<SimpleChatDBContext>() )
+        {
+
+        }
+    }
+}
